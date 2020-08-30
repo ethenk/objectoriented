c9e7512838360b4fa5d72b54f7d8e86c81319449
kisner1720 <spiderbait1720@gmail.com>
2020-08-30 00:08:36 +0000

    test

--- Main.java
+++ Main.java
@@ -7,3 +7,29 @@ class test {
     int A = 100;
   }
 }
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class insertionSort {
+
+public static void main(String[] args) {
+
+    File file = new File("10_Random");
+
+    try {
+
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine()) {
+            int i = sc.nextInt();
+            System.out.println(i);
+        }
+        sc.close();
+    } 
+    catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
+ }
+}
